f43a1e5d77d31a526683b2449dd9bb936865b6b1
oliviapasian <99832146+oliviapasian@users.noreply.github.com>
2022-04-20 12:19:12 -0400

    Add files via upload

---
+++ JavaScript/js.js
@@ -0,0 +1,47 @@
+//this section is for the homepage
+$(document).mousemove(function(){
+    if($(".item1:hover").length == 1){
+        $(".item1").css("background", "cornflowerblue")
+    }
+    else{
+        $(".item1").css("background", "papayawhip")
+    }
+
+    if($(".item2:hover").length == 1){
+        $(".item2").css("background", "cornflowerblue")
+    }
+    else{
+        $(".item2").css("background", "papayawhip")
+    }
+
+    if($(".item3:hover").length == 1){
+        $(".item3").css("background", "cornflowerblue")
+    }
+    else{
+        $(".item3").css("background", "papayawhip")
+    }
+
+    if($(".item4:hover").length == 1){
+        $(".item4").css("background", "cornflowerblue")
+    }
+    else{
+        $(".item4").css("background", "papayawhip")
+    }
+
+    if($(".item5:hover").length == 1){
+        $(".item5").css("background", "cornflowerblue")
+    }
+    else{
+        $(".item5").css("background", "papayawhip")
+    }
+
+    if($(".item6:hover").length == 1){
+        $(".item6").css("background", "cornflowerblue")
+    }
+    else{
+        $(".item6").css("background", "papayawhip")
+    }
+
+})
+
+
